dd7192be5f589a4970678081cb7867106214e253
Léa Pauly <leapauly@macbook-pro-de-lea.home>
2026-02-11 15:37:25 +0100

    feat(home): init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div className="App"> 
+      <h1>Welcome to my React app</h1>
+        <p>Start of a new project</p>
+    </div>
+  )
+}
+
+export default App
