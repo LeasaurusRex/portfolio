3d0a6db90752364a9627b554f4189f2ca170d047
Léa Pauly <leapauly@macbook-pro-de-lea.home>
2026-02-16 16:13:26 +0100

    feat(Home): semantics and fixed footer

--- src/pages/Home/components/StatsWindow.tsx
+++ src/pages/Home/components/StatsWindow.tsx
@@ -5,29 +5,31 @@ import Sticker from "@/shared/Sticker";
 
 function StatsWindow() {
   return (
-    <Window size="md" title="stats.json" footer={<BadgeList />}>
-      <div className={styles.statsContainer}>
-        <Sticker
-          color="yellow"
-          text="2.5"
-          subtext="ANS D'EXPÉRIENCE"
-          rotation="right"
-        />
-        <Sticker
-          color="pink"
-          text="47"
-          subtext="FONCTIONNALITÉS LIVRÉES"
-          rotation="left"
-        />
-        <Sticker
-          color="sky"
-          text="40"
-          subtext="JOURS ETP/MOIS ÉCONOMISÉS"
-          rotation="left"
-        />
-        <Sticker color="white" text="♿️" subtext="A11Y" rotation="right" />
-      </div>
-    </Window>
+    <article>
+      <Window size="md" title="stats.json" footer={<BadgeList />}>
+        <div className={styles.statsContainer}>
+          <Sticker
+            color="yellow"
+            text="2.5"
+            subtext="ANS D'EXPÉRIENCE"
+            rotation="right"
+          />
+          <Sticker
+            color="pink"
+            text="47"
+            subtext="FONCTIONNALITÉS LIVRÉES"
+            rotation="left"
+          />
+          <Sticker
+            color="sky"
+            text="40"
+            subtext="JOURS ETP/MOIS ÉCONOMISÉS"
+            rotation="left"
+          />
+          <Sticker color="white" text="♿️" subtext="A11Y" rotation="right" />
+        </div>
+      </Window>
+    </article>
   );
 }
 
